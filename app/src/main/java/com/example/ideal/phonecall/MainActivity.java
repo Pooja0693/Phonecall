15220de969ed2ba90e50a8284719f6316f727202
parulchopra <parul@ssccglpinnacle.com>
2017-02-14 15:49:02 -0800

    Initial commit

---
+++ app/src/main/java/com/example/ideal/phonecall/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ideal.phonecall;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edittext1;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         edittext1 = (EditText) findViewById(R.id.edittext1);
+         button = (Button) findViewById(R.id.button1);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                String number = edittext1.getText().toString();
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:"+number));
+                if (callIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(callIntent);
+                }
+
+
+            }
+        });
+
+    }
+}
